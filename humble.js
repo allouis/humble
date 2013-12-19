3eb87011c9e85862c4fccebbcf8a3b21644c135b
Fabien <fabienallouis@gmail.com>
2013-12-19 14:42:10 +0000

    Adding mixin, router, advice && observable to humble.js

---
+++ humble.js
@@ -0,0 +1,8 @@
+module.exports = humble;
+
+var humble = {
+    mixin: require("./lib/mixin.js"),
+    router: require("./lib/router.js"),
+    advice: require("./lib/advice.js"),
+    observable: require("./lib/observable.js")
+}
