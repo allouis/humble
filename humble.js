9ff5e673237438b91cdaeeb56e2f7b3190f08a13
Fabien <fabienallouis@gmail.com>
2013-12-24 21:22:16 +0000

    Moving module.exports

--- humble.js
+++ humble.js
@@ -1,8 +1,7 @@
-module.exports = humble;
-
 var humble = {
     mixin: require("./lib/mixin.js"),
     router: require("./lib/router.js"),
     advice: require("./lib/advice.js"),
     observable: require("./lib/observable.js")
 };
+module.exports = humble;
